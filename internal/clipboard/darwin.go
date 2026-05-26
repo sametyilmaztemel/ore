7a5f3220b9b0ab8898d44a68bcaa27734e246631
Samet Yilmaz Temel <sametyilmaztemel@users.noreply.github.com>
2026-05-26 11:20:33 +0300

    feat: add darwin module

--- internal/clipboard/darwin.go
+++ internal/clipboard/darwin.go
@@ -5,3 +5,18 @@ import (
 	"os/exec"
 	"strings"
 )
+
+func readClipboard() string {
+	cmd := exec.Command("pbpaste")
+	out, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+	return strings.TrimRight(string(out), "\n\r")
+}
+
+func writeClipboard(text string) error {
+	cmd := exec.Command("pbcopy")
+	cmd.Stdin = strings.NewReader(text)
+	return cmd.Run()
+}
